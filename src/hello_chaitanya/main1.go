3db4abc4290d6032ff6f0f6fee8e1e63c56f79dd
vurachaitanya <vurachaitanya@gmail.com>
2019-03-23 00:33:37 +0530

    updated

--- src/hello_chaitanya/main1.go
+++ src/hello_chaitanya/main1.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+/* Multi line comments
+learn to explain more about your script
+makes easy to read by a new Dev
+*/
+
+// test to comment single line comments.
+
 func main() {
-	fmt.Println("Hello Chaitanya Vura")
-  
+	fmt.Println("Hello Chaitanya Vura") //trailing comments
+
 }
