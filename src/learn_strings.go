89e61c02fa88a787dcef5ccb2f629c73d94c1634
vurachaitanya <vurachaitanya@gmail.com>
2019-03-23 00:28:49 +0530

    git sample documents

---
+++ src/learn_strings.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Simple String")
+	fmt.Println(` 
+This is a multi-line \n
+
+string
+`)
+	fmt.Println("vura chaitanya")
+	fmt.Println('v')
+	//	fmt.Println('chaitanya')
+	fmt.Println("\u22272")
+}
