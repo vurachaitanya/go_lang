38629f480f9f8e7e84ade1d3c544ea0147bd6a27
Chaitanya Vura <vurachaitanya@gmail.com>
2019-05-01 02:03:39 +0530

    updated code on multi line

---
+++ multi_return_function.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func swap(x, y, z string) (string, string, string) {
+	return z, y, x
+}
+
+func main() {
+	a, b, c := swap("welcome", "chaitu","to go lang")
+	fmt.Println(a, b, c)
+}
