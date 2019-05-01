f473e791acf551fa4e38d6cb50205fbee47a86e4
Chaitanya Vura <vurachaitanya@gmail.com>
2019-05-01 22:42:16 +0530

    updated functions files

---
+++ functions/message.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+func Greeting(name, string) string {
+	return fmt.Sprintf("%s", "name")
+	} 
---
+++ greeting_message.go
@@ -0,0 +1,9 @@
+package main
+import (
+	"fmt"
+	"C:\Users\vchaita3\Chaitanya\go_lang\functions\message"
+)
+func main() {
+	m := message.Greeting("chaitanya")
+	fmt.Println(m)
+}
