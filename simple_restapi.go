adfec28dca9e7d0a6ca93ee9c2ed5af776d04f13
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2021-05-04 21:57:14 +0530

    Create simple_restapi.go

---
+++ simple_restapi.go
@@ -0,0 +1,22 @@
+########## go run simple_restapi.go will create a page which shows "Homepage Endpoint Hit"
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
